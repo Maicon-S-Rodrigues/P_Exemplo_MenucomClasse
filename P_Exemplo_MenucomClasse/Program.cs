3cf4d0d41782be7b4121aa438ddc68304aadb52a
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-08-25 11:32:55 -0300

    Adicionar arquivos de projeto.

---
+++ P_Exemplo_MenucomClasse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P_Exemplo_MenucomClasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
